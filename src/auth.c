5ec0fefa04bbec1c6f436b1eac095cfa6a737497
shfana <wambitafana@gmail.com>
2025-05-27 15:20:48 +0300

    Feat: add registration menu functionality and func signature for  handlingfailed registration

--- src/auth.c
+++ src/auth.c
@@ -97,7 +97,74 @@ int isUsernameUnique(char username[]){
 // registerMenu() handles new user registration, enforces unique usernames, and saves data
 void registerMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]){
 
+ struct termios oflags, nflags;
+
+  // Prompt until unique username is entered
+  do {
+    system("clear");
+    printf("\n\n\t\tBank Management System\n\n\t\t  User Registration\n");
+    printf("\n\n\t\tEnter Username: ");
+    scanf("%s", a);
+
+    if (!isUsernameUnique(a)) {
+      system("clear");
+      printf("\n\n\t\tBank Management System\n\n\t\t  User Registration\n");
+      printf("\n\t\tUsername already exists. \n");
+      handleFailedRegistration(a, pass);
+    }
+  } while (!isUsernameUnique(a));
+
+  // Disable password echo for secure input
+  tcgetattr(fileno(stdin), &oflags);
+  nflags = oflags;
+  nflags.c_lflag &= ~ECHO;
+  nflags.c_lflag |= ECHONL;
+
+  if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
+    perror("tcsetattr");
+    exit(1);
+  }
+  printf("\n\t\tEnter Password: ");
+  scanf("%s", pass);
+
+  if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
+    perror("tcsetattr");
+    exit(1);
+  }
+
+  // Find last user ID
+  FILE *fp;
+  struct User userChecker;
+  char id[MAX_ID_SIZE];
+  int lastUserID = 0;
+
+  if ((fp = fopen("./data/users.txt", "r")) == NULL) {
+    printf("Error! opening file");
+    exit(1);
+  }
+
+  while (fscanf(fp, "%s %s %s", id, userChecker.name, userChecker.password) != EOF) {
+    lastUserID = atoi(id);
+  }
+
+  fclose(fp);
+
+  // Append new user
+  if ((fp = fopen("./data/users.txt", "a")) == NULL) {
+    printf("Error! opening file");
+    exit(1);
+  }
+
+  lastUserID++;
+  fprintf(fp, "%d %s %s\n", lastUserID, a, pass);
+  fclose(fp);
 }
 
+
 // handleFailedLogin() gives users retry or exit options after failed login
-void handleFailedLogin(struct User *u){}
+void handleFailedLogin(struct User *u){
+
+}
+
+// handleFailedRegistration() gives retry or exit option on registration failure
+void handleFailedRegistration(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]) {}
