7e00224d2e0078efeb90d28a83aeee2f6805b938
shfana <wambitafana@gmail.com>
2025-06-11 03:21:04 +0300

    Fix: bugs in authentiction

--- src/auth.c
+++ src/auth.c
@@ -1,105 +1,68 @@
-#include <termios.h>
 #include "header.h"
 
-//constants
-#define MAX_USERNAME_SIZE 50
-#define MAX_PASSWORD_SIZE 50
-#define MAX_ID_SIZE 5
+// Removed constant definitions as they are now in header.h
+// Also removed local definition of USERS_FILE as it's now global in main.c and declared in header.h
 
-char *USERS = "./data/users.txt";
-
-void mainMenu(struct User u);
-
-// loginMenu() collects user credentials with hidden password input
+// loginMenu() collects user credentials (password is now visible)
 void loginMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE])
 {
-    struct termios oflags, nflags;
-
     system("clear");
     printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    printf("\n\t\tEnter Username: ");
     scanf("%s", a);
 
-    // disabling echo
-    tcgetattr(fileno(stdin), &oflags);
-    nflags = oflags;
-    nflags.c_lflag &= ~ECHO;
-    nflags.c_lflag |= ECHONL;
+    printf("\n\t\tEnter the password to login: ");
+    scanf("%s", pass); // Password will be visible
+}
 
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
-    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
-    scanf("%s", pass);
-
-    // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
-
-    printf("\n\t\tEnter the password: ");
-    scanf("%s", pass);
-
-    //restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0){
-        perror("tcsetattr");
-        return exit(1);
-    }
-};
-
-// getPassword() reads stored password for a user from file
-const char *getPassword(struct User u) {
+// getPassword() reads stored password for a user from file and copies it to buffer
+void getPassword(struct User u, char *buffer) {
   FILE *fp;
   struct User userChecker;
-  char id[MAX_ID_SIZE];
+  char id_str[MAX_ID_SIZE]; // Temporary buffer to read ID as string
 
-  if ((fp = fopen("./data/users.txt", "r")) == NULL) {
-    printf("Error! opening file");
+  if ((fp = fopen(USERS_FILE, "r")) == NULL) {
+    perror("\n\t\tError opening users file");
     exit(1);
   }
 
-  while (fscanf(fp, "%s %s %s", id, userChecker.name, userChecker.password) != EOF) {
+  // Read ID as string, then username and password
+  while (fscanf(fp, "%s %s %s", id_str, userChecker.name, userChecker.password) != EOF) {
     if (strcmp(userChecker.name, u.name) == 0) {
+      strcpy(buffer, userChecker.password);
       fclose(fp);
-      u.id = atoi(id); 
-      char *buff = userChecker.password;
-      return buff;
+      return;
     }
   }
 
   fclose(fp);
-  return "no user found"; 
+  strcpy(buffer, "no user found");
 }
 
-//isUsernameUnique() check if  the username already exists
+// isUsernameUnique() checks if the username already exists
 int isUsernameUnique(char username[]){
     FILE *fp;
     struct User userChecker;
+    char id_str[MAX_ID_SIZE];
 
-    if ((fp = fopen("./data/users.txt", "r")) == NULL) {
-        printf("Error! opening file");
+    if ((fp = fopen(USERS_FILE, "r")) == NULL) {
+        perror("\n\t\tError opening users file");
         exit(1);
     }
 
-    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF) {
+    while (fscanf(fp, "%s %s %s", id_str, userChecker.name, userChecker.password) != EOF) {
         if (strcmp(userChecker.name, username) == 0) {
         fclose(fp);
-        return 0; 
+        return 0;
     }
   }
   fclose(fp);
-  return 1; 
+  return 1;
 }
 
 // registerMenu() handles new user registration, enforces unique usernames, and saves data
 void registerMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]){
 
- struct termios oflags, nflags;
-
-  // Prompt until unique username is entered
   do {
     system("clear");
     printf("\n\n\t\tBank Management System\n\n\t\t  User Registration\n");
@@ -109,91 +72,95 @@ void registerMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]){
     if (!isUsernameUnique(a)) {
       system("clear");
       printf("\n\n\t\tBank Management System\n\n\t\t  User Registration\n");
-      printf("\n\t\tUsername already exists. \n");
+      printf("\n\t\tUsername '%s' already exists. \n", a);
       handleFailedRegistration(a, pass);
     }
   } while (!isUsernameUnique(a));
 
-  // Disable password echo for secure input
-  tcgetattr(fileno(stdin), &oflags);
-  nflags = oflags;
-  nflags.c_lflag &= ~ECHO;
-  nflags.c_lflag |= ECHONL;
-
-  if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
-    perror("tcsetattr");
-    exit(1);
-  }
   printf("\n\t\tEnter Password: ");
   scanf("%s", pass);
 
-  if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
-    perror("tcsetattr");
-    exit(1);
-  }
-
-  // Find last user ID
   FILE *fp;
   struct User userChecker;
-  char id[MAX_ID_SIZE];
-  int lastUserID = 0;
+  char id_str[MAX_ID_SIZE];
+  int lastUserID = -1;
 
-  if ((fp = fopen("./data/users.txt", "r")) == NULL) {
-    printf("Error! opening file");
+  if ((fp = fopen(USERS_FILE, "r")) == NULL) {
+    perror("\n\t\tError opening users file for ID check");
     exit(1);
   }
 
-  while (fscanf(fp, "%s %s %s", id, userChecker.name, userChecker.password) != EOF) {
-    lastUserID = atoi(id);
+  while (fscanf(fp, "%s %s %s", id_str, userChecker.name, userChecker.password) != EOF) {
+    lastUserID = atoi(id_str);
   }
-
   fclose(fp);
 
-  // Append new user
-  if ((fp = fopen("./data/users.txt", "a")) == NULL) {
-    printf("Error! opening file");
+  fp = fopen(USERS_FILE, "a");
+  if (fp == NULL) {
+    perror("\n\t\tError opening users file for appending");
     exit(1);
   }
 
   lastUserID++;
   fprintf(fp, "%d %s %s\n", lastUserID, a, pass);
   fclose(fp);
-}
 
+  printf("\n\t\t✔ Registration successful for user '%s'!\n", a);
+}
 
 // handleFailedLogin() gives users retry or exit options after failed login
 void handleFailedLogin(struct User *u){
-int option;
-  do {
-    printf("\n\t\tEnter 0 to try again, 1 to exit! \n\n");
-    scanf("%d", &option);
+    int option;
+    do {
+        printf("\n\t\tEnter 0 to try again, 1 to exit: ");
+        scanf("%d", &option);
 
-    if (option == 0) {
-      initMenu(u); // Retry login
-      break;
-    } else if (option == 1) {
-      exit(1); // Exit program
-      break;
-    } else {
-      printf("Insert a valid operation!\n");
-    }
-  } while (option < 0 || option > 1);
+        if (option == 0) {
+            initMenu(u);
+            return;
+        } else if (option == 1) {
+            exit(1);
+        } else {
+            printf("\t\tInsert a valid operation!\n");
+        }
+    } while (option < 0 || option > 1);
 }
 
 // handleFailedRegistration() gives retry or exit option on registration failure
 void handleFailedRegistration(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]) {
     int option;
-  do {
-    printf("\n\t\tEnter 0 to try again, 1 to exit!\n\n");
-    scanf("%d", &option);
-    if (option == 0) {
-      registerMenu(a, pass); // Retry registration
-      break;
-    } else if (option == 1) {
-      exit(1); // Exit program
-      break;
-    } else {
-      printf("\t\tInsert a valid operation!\n");
+    do {
+        printf("\n\t\tEnter 0 to try again, 1 to exit: ");
+        scanf("%d", &option);
+        if (option == 0) {
+            return;
+        } else if (option == 1) {
+            exit(1);
+        } else {
+            printf("\t\tInsert a valid operation!\n");
+        }
+    } while (option < 0 || option > 1);
+}
+
+// getUserId: Looks up a user ID from the users file based on username
+int getUserId(const char *username) {
+  FILE *fp = fopen(USERS_FILE, "r");
+  if (!fp) {
+    perror("\n\t\tError opening users file");
+    exit(1);
+  }
+
+  char id_str[MAX_ID_SIZE];
+  char name[MAX_USERNAME_SIZE];
+  char pass[MAX_PASSWORD_SIZE];
+
+  while (fscanf(fp, "%s %s %s", id_str, name, pass) != EOF) {
+    if (strcmp(name, username) == 0) {
+      fclose(fp);
+      return atoi(id_str);
     }
-  } while (option < 0 || option > 1);
+  }
+
+  fclose(fp);
+  return -1;
 }
