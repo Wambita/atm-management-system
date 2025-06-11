447f9f89c7bdedfb085295a909ef5005ec83d4e8
shfana <wambitafana@gmail.com>
2025-06-11 03:21:26 +0300

    Fix: auth and vars

--- src/header.h
+++ src/header.h
@@ -1,20 +1,29 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-//constants 
+// Constants
 #define MAX_USERNAME_SIZE 50
 #define MAX_PASSWORD_SIZE 50
 #define MAX_COUNTRY_SIZE 100
 #define MAX_TRANSACTION_TYPE_SIZE 10
+#define MAX_RECORDS 100
+#define MAX_ID_SIZE 5
 
+// File paths - Declared as extern to avoid multiple definitions
+extern const char *USERS_FILE;
+extern const char *RECORDS_FILE;
+
+// Structure for date (used in account records)
 struct Date
 {
     int month, day, year;
 };
 
-// all fields for each record of an account
+// Structure for an account record
 struct Record
 {
     int id;
@@ -26,9 +35,9 @@ struct Record
     int accountNbr;
     double amount;
     struct Date deposit;
-    struct Date withdraw;
 };
 
+// Structure for a user
 struct User
 {
     int id;
@@ -36,19 +45,32 @@ struct User
     char password[MAX_PASSWORD_SIZE];
 };
 
-// authentication functions
+// --- Authentication Functions ---
 void initMenu(struct User *u);
 void loginMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]);
 void registerMenu(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]);
-const char *getPassword(struct User u);
+void getPassword(struct User u, char *buffer);
 void handleFailedLogin(struct User *u);
 void handleFailedRegistration(char a[MAX_USERNAME_SIZE], char pass[MAX_PASSWORD_SIZE]);
+int isUsernameUnique(char username[]);
+int getUserId(const char *username);
 
-// system function
-void createNewAcc(struct User u);
+
+// --- System Functions ---
 void mainMenu(struct User u);
-void checkAllAccounts(struct User u);
+void stayOrReturn(int notGood, void (*f)(struct User), struct User u);
+void success(struct User u);
+int getAccountFromFile(FILE *ptr, struct Record *r);
+void saveAccountToFile(FILE *ptr, struct User u, struct Record r);
+void updateUserAccountInFile(FILE *ptr, struct Record r);
+int doesUserHaveAccounts(struct User u);
+void createNewAcc(struct User u);
 void checkAccountDetails(struct User u);
-void removeAccount(struct User u);
+void checkAllAccounts(struct User u);
+void updateAccountInformation(struct User u);
 void makeTransaction(struct User u);
-void TransferOwnership(struct User u);
+void removeAccount(struct User u);
+void transferOwnership(struct User u);
+
+
+#endif // HEADER_H
